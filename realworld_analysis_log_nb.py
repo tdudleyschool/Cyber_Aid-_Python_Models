5258b0a7f50712238f291ce53d85d202675fa0a3
tdudley501 <tdudley501@gmail.com>
2026-03-29 19:05:30 -0500

    Cyber AID Python Models Version 1.0 (updated instructions to real world analysis) Need: - Needed instructions for other members to edit files.
    Changes:
    - Added instructions to the file "realworld_analysis_log_nb.py" for other members to edit the file and write the test for the real world analysis and risk analysis.
    
    Future Plans:
    - need to acrually code the test for othe real world analysis and risk analysis

---
+++ realworld_analysis_log_nb.py
@@ -0,0 +1,6 @@
+#When writing the test for this dataset this should be similar to the file "log_nb_upload_file_test.py" below are some differences
+# !!!! DO NOT EDIT the code in "log_nb_upload_file_test.py" just use it as a reference for this file since it will be very similar. !!!
+#
+# -you will have to change the path to the demo_realworld_ratio dataset path. model folder should be the same being new models
+# -The load pipeline and image processing stuff should be the same
+# -The evaluation part will mostly be the same. The only metric you will evaluate is the precision. other things don't need to be evaluated. 
